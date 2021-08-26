d4e7e4cae41902b581cb4406f9f2274b34f197c3
Dylan Aubrey <dylanaubrey@gmail.com>
2021-08-26 23:49:17 +0100

    fix: github workflows

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,5 @@ module.exports = {
   ...repodogConfig,
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!**/types.ts', '!**/*.test.*', '!**/__tests__/**'],
   testMatch: ['<rootDir>/src/**/*.test.*'],
-  transformIgnorePatterns: [
-    "node_modules/(?!(unist-util-is|unist-util-visit|unist-util-visit-parents)/)"
-  ]
+  transformIgnorePatterns: ['node_modules/(?!(unist-util-is|unist-util-visit|unist-util-visit-parents)/)'],
 };
